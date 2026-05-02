1fe5848b28d7739b3e9d0d529f130071e5973931
zfrg <zfruiguo2010@163.com>
2026-05-03 00:33:16 +0800

    feat: 更改谱面库

--- src/global.js
+++ src/global.js
@@ -260,7 +260,7 @@ const ptAppInstance = createApp({
                 autoDelay: true,
                 usekwlevelOverbgm: false,
                 resourcesType: "together-pack-1",
-                customChartServer: "ptc.realtvop.top",
+                customChartServer: "ptc.imzfrg.top",
                 prprRespackID: "",
                 enableFilter: false,
                 enableLife: true,
